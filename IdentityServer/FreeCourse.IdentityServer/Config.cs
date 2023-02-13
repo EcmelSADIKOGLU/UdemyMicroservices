654f89f3fdfe886e8e5f1023884d0b74b85c283d
EcmelSADIKOGLU <sadikogluecmel@gmail.com>
2023-02-13 13:33:52 +0300

    discount microservice

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -18,6 +18,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_catalog"){Scopes = {"catalog_fullpermission" } },
             new ApiResource("resource_photo_stock"){Scopes = { "photo_stock_fullpermission" } },
             new ApiResource("resource_basket"){Scopes = { "basket_fullpermission" } },
+            new ApiResource("resource_discount"){Scopes = { "discount_fullpermission" } },
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -43,6 +44,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("catalog_fullpermission","Catalog api için full erişim."),
                 new ApiScope("photo_stock_fullpermission","Photo_stock api için full erişim."),
                 new ApiScope("basket_fullpermission","Basket api için full erişim."),
+                new ApiScope("discount_fullpermission","Discount api için full erişim."),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -72,7 +74,7 @@ namespace FreeCourse.IdentityServer
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
                         IdentityServerConstants.StandardScopes.OfflineAccess,
-                        "roles", "basket_fullpermission"
+                        "roles", "basket_fullpermission","discount_fullpermission"
 
 
                     },
---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountController.cs
@@ -0,0 +1,68 @@
+﻿using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task< IActionResult> GetAll()
+        {
+            var response = await _discountService.GetAll();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var response = await _discountService.GetById(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _sharedIdentityService.GetUserId;
+            var response = await _discountService.GetByCodeAndUserId(code, userId);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            var response = await _discountService.Save(discount);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            var response = await _discountService.Update(discount);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _discountService.Delete(id);
+            return CreateActionResultInstance(response);
+        }
+
+
+    }
+}
